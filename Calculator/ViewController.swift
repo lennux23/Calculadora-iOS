d1827fe5b56dc9d9ffb11217d6dac27e2dd30273
Alejandro Xochihua <alejandroxochihua@MacBook-Pro-de-Alejandro.local>
2015-04-17 22:03:41 -0500

    Mejoras en las funciones

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+
+    var userIsInTheMiddle: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if (userIsInTheMiddle){
+            display.text = display.text! + digit
+            
+        }else{
+            display.text = digit
+            userIsInTheMiddle = true
+            
+        }
     }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if (userIsInTheMiddle){
+            enter()
+        }
+        switch operation {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            case "×": performOperation(multiply)
+            case "−": performOperation({(op1,op2) in op2 - op1 })
+            case "+": performOperation({(op1: Double ,op2: Double) ->Double in return op1 + op2})
+            case "÷": performOperation{$1 / $0}
+            default: break
+        }
+        
     }
-
-
+    
+    func performOperation (operation: (Double,Double) ->Double){
+        if (stackOperation.count >= 2){
+            displayValue =  operation (stackOperation.removeLast() , stackOperation.removeLast())
+            enter()
+            
+        }
+    }
+    
+    
+    func multiply (op1: Double ,op2: Double) ->Double {
+       return op1 * op2
+    }
+    
+    
+    var stackOperation = Array<Double>()
+    
+    // Convierte valores String a Double de forma automatica
+    @IBAction func enter() {
+        userIsInTheMiddle   = false
+        stackOperation.append(displayValue)
+        println("operanStack \(stackOperation)")
+    }
+    
+    
+    // Convierte valores String a Double de forma automatica
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            userIsInTheMiddle   = false
+            display.text = "\(newValue)"
+        }
+    }
 }
 
